cb2f341d5b34d30e8193cfc5c6d3431f0a011050
Steven Foard <stevenfoard@Stevens-MacBook-Pro.local>
2017-07-18 16:05:11 -0600

    first commit

---
+++ exercises/Gocery Store JS/app.js
@@ -0,0 +1,11 @@
+var shopper = {
+    name: 'James',
+    age: 50,
+    hasBeard: true,
+    groceryCart: ['milk,' 'eggs', 'coffee', 'chips'];
+    isHappy: function (){
+        console.log("I am happy");
+    }
+};
+
+shopper.isHappy()
